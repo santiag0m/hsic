fbb692557916c7a211807a6caa525d840c3184f3
santiag0m <32421689+santiag0m@users.noreply.github.com>
2022-03-17 14:02:24 +0100

    Fix parameter shape

--- lib/models/linear_model.py
+++ lib/models/linear_model.py
@@ -10,7 +10,7 @@ class LinearModel(nn.Module):
     ):
         super().__init__()
         self.beta = nn.Linear(in_features, out_features, bias=False)
-        self.bias = nn.Parameter(data=torch.Tensor(0), requires_grad=False)
+        self.bias = nn.Parameter(data=torch.Tensor([0]), requires_grad=False)
 
     def forward(self, x: torch.Tensor) -> torch.Tensor:
         return self.beta(x) + self.bias
