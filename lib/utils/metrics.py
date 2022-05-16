54e4cb2d404f9dcba68b128a60dc3f06f9caa9d6
santiag0m <32421689+santiag0m@users.noreply.github.com>
2022-05-16 19:22:42 +0200

    Add mse + target transform in metrics

--- lib/utils/metrics.py
+++ lib/utils/metrics.py
@@ -1,3 +1,5 @@
+from typing import Callable, Optional
+
 import torch
 import torch.nn as nn
 from torch.utils.data import DataLoader
@@ -20,7 +22,31 @@ def compute_accuracy(model: nn.Module, dataloader: DataLoader) -> torch.Tensor:
     return accuracy.value.item()
 
 
-def compute_bias(model: nn.Module, dataloader: DataLoader) -> torch.Tensor:
+def compute_mse(
+    model: nn.Module,
+    dataloader: DataLoader,
+    target_transform: Optional[Callable] = None,
+) -> torch.Tensor:
+    mse = RunningAverage()
+    device = next(model.parameters()).device
+    with torch.no_grad(), model_eval(model):
+        for inputs, targets in dataloader:
+            inputs = inputs.to(device)
+            targets = targets.to(device)
+            if target_transform is not None:
+                targets = target_transform(targets)
+            preds = model(inputs)
+            batch_mse = (targets - preds) ** 2
+            batch_mse = batch_mse.sum(axis=1)
+            mse.update(batch_mse.cpu())
+    return mse.value.item()
+
+
+def compute_bias(
+    model: nn.Module,
+    dataloader: DataLoader,
+    target_transform: Optional[Callable] = None,
+) -> torch.Tensor:
     target_avg = RunningAverage()
     pred_avg = RunningAverage()
     device = next(model.parameters()).device
@@ -28,8 +54,10 @@ def compute_bias(model: nn.Module, dataloader: DataLoader) -> torch.Tensor:
         for inputs, targets in dataloader:
             inputs = inputs.to(device)
             targets = targets.to(device)
+            if target_transform is not None:
+                targets = target_transform(targets)
             preds = model(inputs)
             target_avg.update(targets)
             pred_avg.update(preds)
-    bias = target_avg.value - pred_avg.value
+    bias = target_avg.value.item() - pred_avg.value.item()
     return bias
