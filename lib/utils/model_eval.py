93061e3bd3a785e4b066b2e319a2a9762bdf3769
santiag0m <32421689+santiag0m@users.noreply.github.com>
2022-05-16 20:02:58 +0200

    Fix context manager

--- lib/utils/model_eval.py
+++ lib/utils/model_eval.py
@@ -2,11 +2,13 @@ import torch.nn as nn
 
 
 class model_eval:
-    def __enter__(self, model: nn.Module):
-        self.is_training = model.training
+    def __init__(self, model: nn.Module):
         self.model = model
+
+    def __enter__(self):
+        self.is_training = self.model.training
         self.model.eval()
 
-    def __exit__(self):
+    def __exit__(self, *args):
         if self.is_training:
             self.model.train()
